da02aa9c9aebfc64194881b203b784950326136c
yuris <yls4@cornell.edu>
2023-10-27 15:19:30 -0400

    fixed setup phase

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import {BrowserRouter} from "react-router-dom"
 import harvardArt from './data/harvardArt';
 import Root from './Root';
 
-ReactDOM.render() => {
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Root />
-    </BrowserRouter>
+    <Root />
   </React.StrictMode>,
-}
+  document.getElementById('root')
+)
